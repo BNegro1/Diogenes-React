76f81d12306cda4e39a1da31787abdfc450a3651
BNegro1 <72948318+BNegro1@users.noreply.github.com>
2024-10-19 19:51:47 -0300

    zzz

--- BuscaVinilos/BuscaVinilos/settings.py
+++ BuscaVinilos/BuscaVinilos/settings.py
@@ -10,8 +10,10 @@ SECRET_KEY = 'django-insecure-eb2toi@qipih2w!x867aojj17#_4x8#)7_va^se%*ifesomv^+
 DEBUG = True
 
 # Asegúrate de agregar tu dominio o nombre en PythonAnywhere
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '.vercel.app'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -55,12 +57,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'BuscaVinilos.wsgi.application'
 
 # Base de datos SQLite
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+
 
 # Archivos estáticos
 STATIC_URL = '/static/'
@@ -81,3 +79,86 @@ USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-eb2toi@qipih2w!x867aojj17#_4x8#)7_va^se%*ifesomv^+'
+
+# En producción, debe estar en False
+DEBUG = True
+
+# Asegúrate de agregar tu dominio o nombre en PythonAnywhere
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tuvinilos.pythonanywhere.com']
+# Dominios - PythonAnywhere
+ALLOWED_HOSTS = ['.vercel.app']
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'webVinilos',  # Tu aplicación
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise para servir archivos estáticos
+]
+
+ROOT_URLCONF = 'BuscaVinilos.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'webVinilos' / 'templates'],  # Aquí defines el directorio de templates
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+
+WSGI_APPLICATION = 'BuscaVinilos.wsgi.application'
+
+DATABASES = {}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Directorio donde están tus archivos estáticos para desarrollo
+STATICFILES_DIRS = [
+    BASE_DIR / "webVinilos" / "static",
+]
+
+# WhiteNoise y archivos estáticos en producción
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Configuración de idioma y zona horaria
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+'''
